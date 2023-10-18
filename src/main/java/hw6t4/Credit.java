6db0e67c03c8c50c48a5a6e90d83d8bb626d197b
barynova <147504845+barynova@users.noreply.github.com>
2023-10-18 17:16:15 +0300

    HW6T4

---
+++ src/main/java/hw6t4/Credit.java
@@ -0,0 +1,100 @@
+package hw6t4;
+
+import java.math.BigDecimal;
+
+public class Credit {
+    public String accountNumber;
+    public BigDecimal body;
+    private double percentage;
+    public int period;
+
+    // Конструктор
+    public Credit(String accountNumber, BigDecimal body, double percentage, String period) {
+        this.accountNumber = accountNumber;
+        this.body = body;
+        this.percentage = percentage;
+        this.period = 6;
+    }
+
+    // Геттер accountNumber
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    // Сеттер accountNumber
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    // Геттер body
+    public BigDecimal getBody() {
+        return body;
+    }
+
+    // Сеттер body
+    public void setBody(BigDecimal body) {
+        this.body = body;
+    }
+
+    // Геттер percentage
+    public double getPercentage() {
+        return percentage;
+    }
+
+    // Сеттер percentage
+    public void setPercentage(double percentage) {
+        this.percentage = percentage;
+    }
+
+    // Геттер period
+    public int getPeriod() {
+        return period;
+    }
+
+    // Сеттер period
+    public void setPeriod(int period) {
+        this.period = period;
+    }
+
+    // Метод щоб показати аккаунт інфо
+    public void displayAccountInfo() {
+        System.out.println("Account Number is: " + accountNumber);
+        System.out.println("Body of credit is: " + body);
+        System.out.println("Percentage of credit: " + percentage + "%");
+    }
+
+    // Метод щоб розрахувати ставку в місяць
+    public double calculatePercentage(double months) {
+        if (months > 0) {
+            double monthlyPercentage = percentage / 12.0;
+            return monthlyPercentage;
+        } else {
+            System.out.println("Percentage is 0.0");
+            return 0.0;
+        }
+
+    }
+    public double calculatePayment(int months) {
+        if (months > 0) {
+            double monthlyPercentage = percentage / 12.0 / 100.0;
+            double monthlyPayment = body.doubleValue() * (monthlyPercentage / (1 - Math.pow(1 + monthlyPercentage, -months)));
+            return monthlyPayment;
+        } else {
+            System.out.println("Invalid number of months.");
+            return 0.0;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Credit creditAccount = new Credit("22111990", new BigDecimal("100.0"), 20.0, "2 months");
+        creditAccount.displayAccountInfo();
+
+        double monthlyPercentage = creditAccount.calculatePercentage(2);
+        System.out.println("Monthly Percentage for 2 months: " + monthlyPercentage + "%");
+
+        double monthlyPayment = creditAccount.calculatePayment(2);
+        System.out.println("Monthly Payment for 2 months: $" + monthlyPayment);
+    }
+}
+
